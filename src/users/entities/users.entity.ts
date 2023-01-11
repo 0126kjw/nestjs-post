557914aecb0cdff92f2c458be636eef621e70ce5
Jinwoo Kim <0126kjw@kakao.com>
2023-01-11 11:12:21 +0900

    Add Users Module

---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,37 @@
+import { Posts } from 'src/posts/entities/posts.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ schema: 'example', name: 'users' })
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { name: 'email', unique: true, length: 30 })
+  email: string;
+
+  @Column('varchar', { name: 'name', length: 20 })
+  name: string;
+
+  @Column('int', { name: 'age' })
+  age: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @OneToMany(() => Posts, (posts) => posts.UserId)
+  OwnedUserPosts: Posts[];
+}
