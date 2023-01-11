b4a2c40b640de60b4b76bbb1379213748d49668e
Jinwoo Kim <0126kjw@kakao.com>
2023-01-11 11:10:01 +0900

    Add Posts Module

---
+++ src/posts/entities/posts.entity.ts
@@ -0,0 +1,42 @@
+import { Users } from 'src/users/entities/users.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ schema: 'example', name: 'posts' })
+export class Posts {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('varchar', { name: 'title', length: '30' })
+  title: string;
+
+  @Column('varchar', { name: 'contents', length: 100 })
+  contents: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @Column('int', { name: 'userId', nullable: false })
+  UserId: number | null;
+
+  @ManyToOne(() => Users, (users) => users.id, {
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'postUserId', referencedColumnName: 'id' }])
+  PostUserId: Users;
+}
